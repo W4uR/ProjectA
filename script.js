007c63402b7e424fadff694ef8f80786a0aeae9f
unknown <abel.kis45682@gmail.com>
2020-09-13 16:53:12 +0200

    Kesz

---
+++ script.js
@@ -0,0 +1,23 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("enlarge")
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+for (let index = 0; index < img.length; index++) {
+    img[index].onclick= function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+      }
+    
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
